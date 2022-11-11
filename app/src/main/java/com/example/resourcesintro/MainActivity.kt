07f49c87c76b4dce13acc87e32afd038a42793d3
Antonio Saracino <antonio.saracino83@gmail.com>
2022-11-12 00:17:49 +0100

    Second commit

--- app/src/main/java/com/example/resourcesintro/MainActivity.kt
+++ app/src/main/java/com/example/resourcesintro/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.resourcesintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.handleCoroutineException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingView : TextView
+    val name = "Antonio"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingView = findViewById(R.id.textView)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener() {
+            handleButtonPress()
+        }
+
     }
+
+    fun handleButtonPress() {
+        greetingView.text = getString(R.string.button_pressed_text, name)
+    }
+
+
 }
